ca196b07bfd67d79acb132fcb103da785b3b7691
reyna <queen_niimue@live.com.mx>
2014-02-20 17:30:27 -0600

    Pk2

---
+++ PK1_PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PK1_PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Buenos Días !!!" : "Buenas Tardes !!!";
+            return View();
+        }
+        //Get: /Home/RsvpForm
+        //Renderea el Link de la forma del RSVP
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+    }
+}
