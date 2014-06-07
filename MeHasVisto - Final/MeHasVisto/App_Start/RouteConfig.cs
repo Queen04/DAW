5bedb93d8950b309ba2de835c03070434c2a6eaf
reyna <queen_niimue@live.com.mx>
2014-06-07 10:47:17 -0500

    MehasVisto - Final

---
+++ MeHasVisto - Final/MeHasVisto/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MeHasVisto
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //es una plantilla de ruteo y es la Queryable define como ban as estar definidas las peticiones http en los controladores
+            routes.MapRoute(
+                name: "Default", //
+                url: "{controller}/{action}/{id}", // patron de la ruta, identifica el nombre del controlador, nombre de la accion, parametros del http
+              defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional
+         }
+            );
+        }
+    }
+}
---
+++ MeHasVisto - Final/MeHasVisto/Controllers/PetController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeHasVisto.Controllers
+{
+    public class PetController : Controller
+    {
+        //
+        // GET: /Pet/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Display()
+        {
+            var name = (string)RouteData.Values["id"];
+            //var cont = (string)RouteData.Values["controller"];
+            //var act = (string)RouteData.Values["action"];
+            //return name+cont+act;
+            object model= null;
+            if (model == null)
+                return RedirectToAction("NotFound");
+            return View(); //return View(model);
+        }
+
+        public ActionResult NotFount()
+        {
+            //ViewData["ErrorCode"] = "NFE0001";
+            //ViewData["Description"] = "La mascota no se encuentra" + "en la base de datos";
+            ViewBag.ErrorCode = "NFE0001";
+            ViewBag.Description = "La mascota no se encuentra" + "en la base de datos";
+            return View();
+        }
+
+        public FileResult DownLoadPicture()
+        {
+            var name = (string)RouteData.Values["id"];
+            var picture =@"C:\Users\USUARIO\Pictures\foOtiicoOs\pick´s/" + name + ".jpg";
+            var contentType = "image/jpg";
+            var fileName = name + ".jpg";
+            return File(picture, contentType, fileName);
+
+        }
+
+        public HttpStatusCodeResult UnauthorizedError()
+        {
+            return new HttpUnauthorizedResult("Error de acceso no autorizado");
+        }
+
+        public ActionResult NotFoundError()
+        {
+            return HttpNotFound("Nada por aqui ...");
+        }
+
+        public ActionResult PictureUpload()
+        {
+            return View();
+        }
+    }
+}
